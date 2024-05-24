372c456e3f3dc48b9233e4ecbabd3b37fcb2c465
kim <yahyunjin5@gmail.com>
2024-05-25 00:06:59 +0900

    알고리즘 수행시간 6

--- src/main/BaekJoon/11_시간 복잡도/n24267.java
+++ src/main/BaekJoon/11_시간 복잡도/n24267.java
@@ -1,13 +1,15 @@
-// 15784 KB , 140 ms
+// 15676 KB , 140 ms
 /*
  MenOfPassion(A[], n) {
     sum <- 0;
-    for i <- 1 to n
-        for j <- 1 to n
-           sum <- sum + A[i] x A[j];
+    for i <- 1 to n - 2
+        for j <- i + 1 to n - 1
+            for k <- j + 1 to n
+               sum <- sum + A[i] x A[j] x A[k];
     return sum;
  }
- n 을 입력받아 n 까지 반복하는 행위가 세 번 중첩된다. 수행 횟수는 n * n * n 이고 시간복잡도는 O(n^3), 차수 3이다.
+ 크기 순으로 중복되지 않게 뽑는 경우의 수와 같다. 즉 조합을 구하는 방식이 수행 횟수이다.
+ n 개의 수 중 3 개를 뽑으므로 nC3 = ((n * (n - 1) * (n - 2)) / 3! 이고, 시간복잡도는 O(n^3), 차수 3이다.
  3 제곱을 표현하기 위해 자료형은 최소 long 이어야 한다
  */
 
@@ -21,8 +23,7 @@ public class Main {
         long n = Long.parseLong(br.readLine());
         br.close();
 
-        bw.write(n * n * n  + "\n");
-        bw.write("3" + "\n");
+        bw.write(((n * (n - 1) * (n - 2)) / 6)  + "\n" + 3);
 
         bw.flush();
         bw.close();
