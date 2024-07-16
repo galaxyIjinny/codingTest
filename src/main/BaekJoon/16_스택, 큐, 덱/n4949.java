1d7052024d15f7bef79edd9936e64072e1ce2478
kim <yahyunjin5@gmail.com>
2024-07-16 23:42:51 +0900

    균형잡힌 세상 : 스택 클래스 사용, 괄호 확장

---
+++ src/main/BaekJoon/16_스택, 큐, 덱/n4949.java
@@ -0,0 +1,62 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s;
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            s = br.readLine();
+            if (s.equals(".")) {
+                break;
+            }
+
+            sb.append(solve(s)).append('\n');
+        }
+
+        br.close();
+        bw.write(sb.toString());
+
+        bw.flush();
+        bw.close();
+    }
+
+    public static String solve(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '(' || c == '[') {
+                stack.push(c);
+            }
+
+            else if (c == ')') {
+                if (stack.empty() || stack.peek() != '(') {
+                    return "no";
+                } else {
+                    stack.pop();
+                }
+            }
+
+            else if (c == ']') {
+                if (stack.empty() || stack.peek() != '[') {
+                    return "no";
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+
+        if (stack.empty()) {
+            return "yes";
+        } else {
+            return "no";
+        }
+    }
+}
